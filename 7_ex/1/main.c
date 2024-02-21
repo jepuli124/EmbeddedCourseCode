2081ee81ef568118fbfcf7d655c2c24a2f97b7d4
Dorfultariant <tunxero@hotmail.com>
2024-02-21 09:55:06 +0200

    led blinks at interval via interrupt ISR

--- 7_ex/1/main.c
+++ 7_ex/1/main.c
@@ -1,82 +1,88 @@
 #define F_CPU 16000000UL
+
 #define NOTE_C5 523
 #define NOTE_A7 3520
 #define NOTE_B2 123
+#define LED_BLINK 10
 
 #include <avr/io.h>
-#include <util/delay.h>
 #include <stdlib.h>
+#include <util/delay.h>
 // You will need to include the following library: <avr/interrupt.h>.
 #include <avr/interrupt.h>
 
-#include "lcd.h"
+// Connect a passive buzzer to digital pin 9 of the UNO and set the DDRx as output.
+const int BUZZER = PB1;
+const int BUILTIN = PB5;
 
+const int PRESCALER_64 = 64;
+const int PRESCALER_1 = 1;
 
+volatile int cnt = 0;
 
-const int DATA_OUTPUT = PD9;
-const int PRESCALER64 = 64;
-const int PRESCALER1 = 1;
-int COUNTER = 0; //counts interupts ?
-
-int count(char* p);
-
-uint16_t counter = 0;
-// When the ADC is complete, print the result in the LCD.
-// You can save the conversion results in the ISR(interrupt_vector) sub-routine 
-// ISR for timer/counter1 overflow
-ISR(interrupt_vector) {
+ISR(TIMER1_COMPA_vect) {
   // Reset the timer/counter1 to maintain the PWM frequency
   TCNT1 = 0;
+  // if (cnt >= 50) {
+  PORTB ^= (1 << BUILTIN);  
+  // }
+  // cnt = (cnt >= 50) ? cnt = 0 : ++cnt;
+  // Debug led toggle
+  
+}
+
+int main(void) {
+
+  // Zero registers
+  TCCR1B = 0;
+  OCR1A = 0;
+
+  // Buzzer and builtin led output
+  DDRB |= (1 << BUZZER);
+  DDRB |= (1 << BUILTIN);
+
+
+  // Set COM1A0 mode to compare match with OC1A
+  // Set waveform generation mode to PWM, phase and frequency correct and TOP as
+  // OCR1A with WGM13 WGM10
+  //TCCR1A = (1 << COM1A0) | (1 << WGM10);
+
+  TCCR1A = (1 << 6); // COM1A0
+  TCCR1A = (1 << 0); // WGM10
+
+  // TCCR1B = (1 << 4); // WGM13
+  TCCR1B = (1 << CS10) | (1 << WGM13) | (1 << WGM10);
+
+  // Enable COMPA interrupt
+  TIMSK1 = (1 << OCIE1A);
+
+  TCNT1 = 0;
+
+  // Toggling builtin led works, lower note -> lower light output,
+  //     higher note -> higher light output
+  OCR1A = F_CPU / (2 * PRESCALER_1 * LED_BLINK);
+  
+  // TCCR1B = (1 << CS10) | (1 << CS11) | (1 << WGM13) | (1 << WGM10); // Prescale 64
+  // OCR1A = F_CPU / (2 * PRESCALER_64 * NOTE_B2);
+
+  sei();
+  while (1) {
+    // Play note C5
+    ;;
+    // // Play note A7
+    // OCR1A = F_CPU / (2 * PRESCALER_1 * NOTE_A7);
+    //continue;
+    // // Play note B2
+    
+  }
+
+  return 0;
 }
 
 
-int main(void) { 
- 
-    //Empty values
-    TCNT1 = 0;
-    TCCR1B = 0;
-
-    OC1A = 0; 
-
-    //DDRx input !TODO check that is correct port setting
-    DDRD |= (1 << DATA_OUTPUT);
-
-    // Set COM1A0 mode to compare match with OC1A
-    // Set waveform generation mode to PWM, phase and frequency correct and TOP as OCR1A with WGM13 WGM10
-    TCCR1A = (1 << COM1A0) | (1 << WGM13) | (1 << WGM10);
-    
-    // Enable the PWM output by setting the clock source (prescaler 1) should be same as constant PRESCALER1.
-    // !TODO tips said to add WGM13 and WGM10 as ti actiwave mode 9.
-    TCCR1B =  (1 << CS10) | (1 << WGM13) | (1 << WGM10);
-    
-    // Enable global timer/counters interrupts
-    TIMSK1 = (1 << TOIE1);
-
-    sei();
-
-    
-
-    while(1){
-
-    TCCR1B =  (1 << CS10) | (1 << WGM13) | (1 << WGM10);
-    // Note set
-    OCR1A = F_CPU/(2*PRESCALER1*NOTE_C5);
-    
-
-    delay(1000); // Play the note for 1 second
-
-    // Note set
-    OCR1A = F_CPU/(2*PRESCALER1*NOTE_A7);
-    
-
-    delay(1000); // Play the note for 1 second
-
-    // Enable the PWM output by setting the clock source (prescaler 64) should be same as constant PRESCALER64.
-    TCCR1B = (1 << CS11) | (1 << CS10) | (1 << WGM13) | (1 << WGM10);
-    OCR1A = F_CPU/(2*PRESCALER64*NOTE_B2);
-
-    }
-    
-    return 0;
-}
-
+    // // Note set
+    // OCR1A = F_CPU / (2 * PRESCALER1 * NOTE_A7);
+    // Enable the PWM output by setting the clock source (prescaler 64) should
+    // be same as constant PRESCALER64.
+    // TCCR1B = (1 << CS11) | (1 << CS10) | (1 << WGM13) | (1 << WGM10);
+    // OCR1A = F_CPU / (2 * PRESCALER_64 * NOTE_B2);
