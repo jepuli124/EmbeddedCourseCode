a8caa95f232b18b9f5036d36484c89984764c185
Dorfultariant <tunxero@hotmail.com>
2024-02-21 10:21:29 +0200

    Note order set C5 A7 B2

--- 7_ex/1/main.c
+++ 7_ex/1/main.c
@@ -47,33 +47,49 @@ int main(void) {
   // OCR1A with WGM13 WGM10
   //TCCR1A = (1 << COM1A0) | (1 << WGM10);
 
-  TCCR1A = (1 << 6); // COM1A0
-  TCCR1A = (1 << 0); // WGM10
+  TCCR1A = (1 << COM1A0); // COM1A0
+  TCCR1A = (1 << WGM10); // WGM10
 
   // TCCR1B = (1 << 4); // WGM13
-  TCCR1B = (1 << CS10) | (1 << WGM13) | (1 << WGM10);
+  // Prescaler 1
+  TCCR1B = (1 << CS10) | (1 << WGM13);
 
   // Enable COMPA interrupt
   TIMSK1 = (1 << OCIE1A);
 
-  TCNT1 = 0;
 
   // Toggling builtin led works, lower note -> lower light output,
   //     higher note -> higher light output
-  OCR1A = F_CPU / (2 * PRESCALER_1 * LED_BLINK);
+  // Sets the 
+  TCNT1 = 0;
+  
+  //OCR1A = F_CPU / (2 * PRESCALER_1 * LED_BLINK);
   
   // TCCR1B = (1 << CS10) | (1 << CS11) | (1 << WGM13) | (1 << WGM10); // Prescale 64
   // OCR1A = F_CPU / (2 * PRESCALER_64 * NOTE_B2);
 
   sei();
+
   while (1) {
-    // Play note C5
-    ;;
-    // // Play note A7
-    // OCR1A = F_CPU / (2 * PRESCALER_1 * NOTE_A7);
-    //continue;
-    // // Play note B2
+    // Reset to 0 and set prescaler 1
+    TCCR1B = 0;
+    TCCR1B = (1 << CS10) | (1 << WGM13);
     
+    OCR1A = F_CPU / (2 * PRESCALER_1 * NOTE_C5);
+
+    _delay_ms(500);
+
+    OCR1A = F_CPU / (2 * PRESCALER_1 * NOTE_A7);
+
+    _delay_ms(750);
+
+    // Enable the PWM output by setting the clock source (prescaler 64) should
+    // be same as constant PRESCALER64.
+    TCCR1B = (1 << CS11) | (1 << CS10);
+    OCR1A = F_CPU / (2 * PRESCALER_64 * NOTE_B2);
+    
+    _delay_ms(500);
+
   }
 
   return 0;
