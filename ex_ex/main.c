1d9d97f4183db098a39534ca3a12fdb56976d26a
jepuli124 <konsta.jalkanen.kj@gmail.com>
2024-04-06 10:07:21 +0300

    added the buzzer from other weekly ex

--- ex_ex/main.c
+++ ex_ex/main.c
@@ -3,15 +3,20 @@
 #define FOSC 16000000UL
 #define BAUD 9600
 #define MYUBRR FOSC/16/BAUD-1
-
+#define NOTE_C5 523
+#define NOTE_A7 3520
+#define NOTE_B2 123
 
 #include <avr/io.h>
 #include <util/delay.h>
 #include <util/setbaud.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <avr/interrupt.h>
 
 const int LED_BUILTIN = PB5;
+const int PRESCALER_64 = 64;
+const int PRESCALER_1 = 1;
 
 static int uart_putchar(char c, FILE* stream);
 static int uart_readchar(FILE* stream);
@@ -54,6 +59,32 @@ int main(void) {
     stdout = &mystdout;
     stdin = &mystdin;
 
+    TCCR1B = 0;
+    OCR1A = 0;
+
+    // Buzzer and builtin led output
+    DDRB |= (1 << BUZZER);
+    DDRB |= (1 << BUILTIN);
+
+
+    TCCR1A = (1 << COM1A0); // COM1A0
+    TCCR1A = (1 << WGM10); // WGM10
+
+    // Prescaler 1
+    TCCR1B = (1 << CS10) | (1 << WGM13);
+
+    // Enable COMPA interrupt
+    TIMSK1 = (1 << OCIE1A);
+
+
+    // Toggling builtin led works, lower note -> lower light output,
+    //     higher note -> higher light output
+    // Sets the 
+    TCNT1 = 0;
+    
+
+    sei();
+
     while(12) {
         fprintf(stdin, "%s\n", "Hello world!");
         printf("Hello there!");
@@ -61,6 +92,30 @@ int main(void) {
         _delay_ms(250);
         PORTB |= (1 << LED_BUILTIN);
         _delay_ms(250);
+    }
+
+    
+
+    while (1) {
+        // Reset to 0 and set prescaler 1
+        TCCR1B = 0;
+        TCCR1B = (1 << CS10) | (1 << WGM13);
+        
+        OCR1A = F_CPU / (2 * PRESCALER_1 * NOTE_C5);
+
+        _delay_ms(500);
+
+        OCR1A = F_CPU / (2 * PRESCALER_1 * NOTE_A7);
+
+        _delay_ms(750);
+
+        // Enable the PWM output by setting the clock source (prescaler 64) should
+        // be same as constant PRESCALER64.
+        TCCR1B = (1 << CS11) | (1 << CS10);
+        OCR1A = F_CPU / (2 * PRESCALER_64 * NOTE_B2);
+        
+        _delay_ms(500);
+
     }
 
     return 0;
